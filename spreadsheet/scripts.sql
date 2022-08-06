14dd8f29eee4a7be71316674183a9fbed43937ef
Robert Lunn <robertrlunn@gmail.com>
2022-08-06 13:55:20 -0500

    finished

--- spreadsheet/scripts.sql
+++ spreadsheet/scripts.sql
@@ -1,26 +1,101 @@
 /* multiline comment uses these symbols */
 -- single line comments like this
 
+-- 1.How many rows are in the data_analyst_jobs table? 
 -- SELECT COUNT (*) FROM data_analyst_jobs; 
 -- Q1 - 1793 ROWS
 
+-- 2.Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row? 
 -- SELECT * FROM data_analyst_jobs LIMIT 10;
 -- Q2 - ExxonMobil
 
+/* 3.How many postings are in Tennessee? 
+How many are there in either Tennessee or Kentucky? */
 /* SELECT COUNT(location) FROM data_analyst_jobs
 WHERE location = 'TN'; */
 -- Q3 - 21 in TN
-
 /* SELECT COUNT(location) FROM data_analyst_jobs
 WHERE location = 'TN' OR location = 'KY'; */
 -- Q3 - 27 in TN/KY
 
+--4.How many postings in Tennessee have a star rating above 4?
 /* SELECT * FROM data_analyst_jobs WHERE star_rating > 4 
 AND location = 'TN'; */
 -- Q4 - 3
 
+--5.How many postings in the dataset have a review count between 500 and 1000? 
 /* SELECT * FROM data_analyst_jobs 
 WHERE review_count BETWEEN 500 AND 1000; */
 -- Q5 - 151
+
+/* 6.Show the average star rating for companies in each state.
+The output should show the state as `state` and the average rating for the state as `avg_rating`. 
+Which state shows the highest average rating?  */
+/* SELECT location AS state, AVG(star_rating) AS avg_rating 
+FROM data_analyst_jobs
+WHERE star_rating IS NOT NULL
+GROUP BY location
+ORDER BY avg_rating DESC; */
+-- Q6 - Nebraska
+
+/* 7.Select unique job titles from the data_analyst_jobs table. 
+How many are there? */
+/* SELECT DISTINCT(title)
+FROM data_analyst_jobs; */
+-- Q7 - 881
+
+-- 8.How many unique job titles are there for California companies?
+/* SELECT DISTINCT(title)
+FROM data_analyst_jobs
+WHERE location = 'CA'; */
+-- Q8 - 230
+
+/* 9.Find the name of each company and its average star rating for all companies that have more than
+5000 reviews across all locations.
+How many companies are there with more that 5000 reviews across all locations? */
+/* SELECT company, AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs
+WHERE review_count > 5000 AND company IS NOT NULL
+GROUP BY company, review_count; */
+-- Q9 - 45
+
+
+/* 10.Add the code to order the query in #9 from highest to lowest average star rating. 
+Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? 
+What is that rating? */
+/* SELECT company, AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs
+WHERE review_count > 5000 AND company IS NOT NULL
+GROUP BY company, review_count
+ORDER BY avg_rating DESC; */
+-- Q10 - American Express, 4.19 rating
+
+/* 11.Find all the job titles that contain the word â€˜Analystâ€™. How many different job titles are there? */
+/*SELECT COUNT(DISTINCT title)
+FROM data_analyst_jobs
+WHERE title LIKE '%_nalyst%' OR title LIKE '%_NALYST%'; */
+-- Q11 - 774
+
+/* 12.How many different job titles do not contain either the word â€˜Analystâ€™ or the word â€˜Analyticsâ€™? 
+What word do these positions have in common? */
+/* SELECT DISTINCT(title)
+FROM data_analyst_jobs
+WHERE title NOT LIKE '%ANALY%' AND title NOT LIKE '%_naly%'; */
+-- Q12 - 4 titles, all involve Tableau
+
+/* **BONUS:**
+You want to understand which jobs requiring SQL are hard to fill. 
+Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
+ - Disregard any postings where the domain is NULL. 
+ - Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top. 
+  - Which three industries are in the top 4 on this list? How many jobs have been listed for more than 3 weeks for each of the top 4? */
+/* SELECT COUNT(domain), domain, days_since_posting, skill
+FROM data_analyst_jobs
+WHERE skill LIKE '%SQL%' AND domain IS NOT NULL AND days_since_posting > 21
+GROUP BY domain, days_since_posting, skill
+ORDER BY count DESC; */
+-- Banks and Financial Services - 6 
+-- Consulting and Business Services - 7
+-- Insurance Health Care - 3
 
 
